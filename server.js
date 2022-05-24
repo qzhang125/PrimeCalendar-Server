99da9ebad7c343ecd1b9733c7efc9045c9440b46
Qiuyu Zhang <qzhang125@myseneca.ca>
2022-05-24 13:39:15 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const cors = require("cors");
+const jwt = require('jsonwebtoken');
+const passport = require("passport");
+const passportJWT = require("passport-jwt");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const userService = require("./service.js");
+
+const app = express();
+
+const HTTP_PORT = process.env.PORT || 8080;
+
+
+var ExtractJwt = passportJWT.ExtractJwt;
+var JwtStrategy = passportJWT.Strategy;
+
+var jwtOptions = {};
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
+jwtOptions.secretOrKey = process.env.JWT_SECRET;
+var strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
+    console.log('payload received', jwt_payload);
+
+    if (jwt_payload) {
+        next(null, { _id: jwt_payload._id, 
+            userName: jwt_payload.userName, 
+            password: jwt_payload.password}); 
+    } else {
+        next(null, false);
+    }
+});
+passport.use(strategy);
+app.use(passport.initialize());
+app.use(express.json());
+app.use(cors());
+
+/* TODO Add Your Routes Here */
+app.post("/api/user/register",(req, res)=>{
+    userService.registerUser(req.body).then(()=>{
+        res.json({message: "Resolve sucessful"});
+    }).catch(err=>{
+        res.status(422).json({message: err});
+    });
+});
+
+app.post("/api/user/login",(req,res)=>{
+    userService.checkUser(req.body).then(user=>{
+        let token = jwt.sign({
+            _id: user._id,
+            userName: user.userName
+        }, jwtOptions.secretOrKey);
+        res.json({message: "User Logged In", token: token});
+    }).catch(err=>{
+        console.log(err);
+        res.status(422).json({message: err});
+    });
+});
+
+userService.connect()
+.then(() => {
+    app.listen(HTTP_PORT, () => { console.log("API listening on: " + HTTP_PORT) });
+})
+.catch((err) => {
+    console.log("unable to start the server: " + err);
+    process.exit();
+});
---
+++ service.js
@@ -0,0 +1,84 @@
+const mongoose = require('mongoose');
+//mongoose.set('useFindAndModify', false); //https://mongoosejs.com/docs/deprecations.html#findandmodify
+const bcrypt = require('bcryptjs');
+
+let mongoDBConnectionString = process.env.MONGO_URL;
+
+let Schema = mongoose.Schema;
+
+let userSchema = new Schema({
+    userName: {
+        type: String,
+        unique: true
+    },
+    password: String,
+    favourites: [String]
+});
+
+let User;
+
+module.exports.connect = function () {
+    return new Promise(function (resolve, reject) {
+        let db = mongoose.createConnection(mongoDBConnectionString, { useUnifiedTopology: true });
+
+        db.on('error', err => {
+            reject(err);
+        });
+
+        db.once('open', () => {
+            User = db.model("users", userSchema);
+            resolve();
+        });
+    });
+};
+
+module.exports.registerUser = function (userData) {
+    return new Promise(function (resolve, reject) {
+
+        if (userData.password != userData.password2) {
+            reject("Passwords do not match");
+        } else {
+
+            bcrypt.hash(userData.password, 10).then(hash => {
+
+                userData.password = hash;
+
+                let newUser = new User(userData);
+
+                newUser.save(err => {
+                    if (err) {
+                        if (err.code == 11000) {
+                            reject("User Name already taken");
+                        } else {
+                            reject("There was an error creating the user: " + err);
+                        }
+
+                    } else {
+                        resolve("User " + userData.userName + " successfully registered");
+                    }
+                });
+            })
+                .catch(err => reject(err));
+        }
+    });
+};
+
+module.exports.checkUser = function (userData) {
+    return new Promise(function (resolve, reject) {
+
+        User.findOne({ userName: userData.userName })
+            .exec()
+            .then(user => {
+                bcrypt.compare(userData.password, user.password).then(res => {
+                    if (res === true) {
+                        resolve(user);
+                    } else {
+                        reject("Incorrect password for user " + userData.userName);
+                    }
+                });
+            }).catch(err => {
+                console.log(err);
+                reject("Unable to find user " + userData.userName);
+            });
+    });
+};
